ce93f86f22b37e7fd669549c6ba5b17c036dfefd
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-24 02:27:01 -0500

    create UI to write text

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
         </p>
     </body>
@@ -38,9 +39,7 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
         if(queryData.id === undefined){
-
             fs.readdir('./data', function(error, filelist){
-                console.log(filelist)
                 var title = 'Welcome';
                 var description = 'Hello, Node.js'; 
                 var list = templateList(filelist);
@@ -59,6 +58,25 @@ var app = http.createServer(function(request,response){
                 });
             });
         }
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+                <form action="http://localhost:3000/process_create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+                </form>
+          `);
+            response.writeHead(200);
+            response.end(template);
+        });
+
     } else {
         response.writeHead(404);
         response.end('Not Found');
